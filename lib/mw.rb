167d4841f436f7b0efbb8a12025320926c960d63
Edward Richards <erichards@lingotek.com>
2016-06-24 08:33:28 -0600

    added default change back to home directory at the beginning of each system call to prevent missing folder error

--- lib/mw.rb
+++ lib/mw.rb
@@ -22,36 +22,44 @@ module Mw
     end
 
     def start_mw_server
+      Dir.chdir("#{Dir.home}")
       Dir.chdir("#{@@config[:mw_path]}")
       system 'gnome-terminal -e "php artisan serve"'
     end
 
     def start_queue
+      Dir.chdir("#{Dir.home}")
       Dir.chdir("#{@@config[:mw_path]}")
       system 'gnome-terminal -e "php artisan queue:listen --tries=1"'
     end
 
     def start_ngrok
+      Dir.chdir("#{Dir.home}")
       Dir.chdir("#{@@config[:ngrok_path]}")
       system 'gnome-terminal -e "./ngrok http 8000"'
     end
 
     def open_directory
+      Dir.chdir("#{Dir.home}")
       Dir.chdir("#{@@config[:mw_path]}")
       system 'gnome-terminal'
     end
 
     def vagrant_start
+      Dir.chdir("#{Dir.home}")
+      puts Dir.pwd
       Dir.chdir("#{@@config[:vagrant_path]}")
       system 'vagrant up'
     end
 
     def vagrant_intialize
+      Dir.chdir("#{Dir.home}")
       Dir.chdir("#{@@config[:vagrant_path]}")
       system 'vagrant ssh -- -t "cd Code/mw; php artisan migrate; php artisan db:seed"'
     end
 
     def vagrant_queue
+      Dir.chdir("#{Dir.home}")
       Dir.chdir("#{@@config[:vagrant_path]}")
       system 'echo Starting Queue...'
       system 'echo Queue Running...; echo ""'
